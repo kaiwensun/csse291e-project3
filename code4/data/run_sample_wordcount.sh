95c886688d16c78bfe4941199cd4f82c67a807f5
kaiwensun <skw_kevin@126.com>
2016-05-28 03:16:36 -0700

    run woudcounter on one master and one slave

--- code4/data/run_sample_wordcount.sh
+++ code4/data/run_sample_wordcount.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 SIGN='\033[1;42mKevin:\033[0m'
-cd WordCount
+cd /data/WordCount
 
 #In this docker image, there is already an /input/ directory in HDFS. Don't touch it.
 printf "${SIGN} Making a directory at the hadoop file system.\n"
-hadoop fs -mkdir -p myinput
+$HADOOP_PREFIX/bin/hadoop fs -mkdir -p myinput
 
 printf "${SIGN} Uploading the input files to hadoop file system. the -put flag is equivalent to -copyFromLocal. The -f flag forces overwrite.\n"
-hadoop fs -put -f file*.txt myinput
+$HADOOP_PREFIX/bin/hadoop fs -put -f file*.txt myinput
 
 printf "${SIGN} Removing myoutput directory. The output should not exist.\n"
-hadoop fs -rm -r -f -skipTrash myoutput
+$HADOOP_PREFIX/bin/hadoop fs -rm -r -f -skipTrash myoutput
 
 printf "${SIGN} Current files in hadoop file system are:\n"
-hadoop fs -ls myinput
+$HADOOP_PREFIX/bin/hadoop fs -ls myinput
 
 printf "${SIGN} Compiling WordCount.java\n"
-hadoop com.sun.tools.javac.Main WordCount.java
+$HADOOP_PREFIX/bin/hadoop com.sun.tools.javac.Main WordCount.java
 
 printf "${SIGN} Creating wc.jar file\n"
 jar cf wc.jar WordCount*.class
 
 printf "${SIGN} Run wc.jar using hadoop\n"
-hadoop jar wc.jar WordCount /user/root/myinput /user/root/myoutput
+$HADOOP_PREFIX/bin/hadoop jar wc.jar WordCount /user/root/myinput /user/root/myoutput
 
 printf "${SIGN} Fetching the result from HDFS:\n"
-hadoop fs -cat myoutput/part-r-00000
+$HADOOP_PREFIX/bin/hadoop fs -cat myoutput/part-r-00000
--- code4/start.sh
+++ code4/start.sh
@@ -2,7 +2,12 @@
 
 SIGN='\033[1;42mKevin:\033[0m'
 PWD=$(pwd)
+XML='/data/xml/core-site.xml  /data/xml/hdfs-site.xml  /data/xml/mapred-site.xml  /data/xml/masters  /data/xml/slaves'
 
+
+
+printf "${SIGN} Making sure docker containers don't conflict\n"
+${PWD}/clean.sh
 
 printf "${SIGN} Creating network bridge\n"
 docker network create -d bridge  my-bridge-network
@@ -11,12 +16,31 @@ docker network create -d bridge  my-bridge-network
 #	docker pull sequenceiq/hadoop-docker:2.7.0
 
 printf "${SIGN} Creating containers\n"
-docker run -v ${PWD}/data:/data --name Master --net my-bridge-network -td sequenceiq/hadoop-docker:2.7.0 /etc/bootstrap.sh -bash
-docker run -v ${PWD}/data:/data --name Slave1 --net my-bridge-network -td sequenceiq/hadoop-docker:2.7.0 /etc/bootstrap.sh -bash
+docker run -v ${PWD}/data:/data --name Master --net my-bridge-network -td sequenceiq/hadoop-docker:2.7.0 bash
+docker run -v ${PWD}/data:/data --name Slave1 --net my-bridge-network -td sequenceiq/hadoop-docker:2.7.0 bash
 #
 #For the following times, you've already have a container hadoopDocker. So simply start it
 #docker start -i hadoopDocker
 
-printf "${SIGN} Configuring Master's slaves\n"
-docker exec Master "echo Slave1 > \$HADOOP_PREFIX/etc/hadoop/slaves"
 
+printf "${SIGN} Starting SSH connection\n"
+docker exec -t Master service sshd start
+docker exec -t Slave1 service sshd start
+
+
+printf "${SIGN} Creating meta directories for naming and data at Master\n"
+docker exec -t Master mkdir -p /usr/local/hadoop/hdfs/name
+docker exec -t Master mkdir -p /usr/local/hadoop/hdfs/data
+
+printf "${SIGN} Copying hadoop config files to master and slaves\n"
+docker exec -t Master cp ${XML} /usr/local/hadoop/etc/hadoop/
+docker exec -t Slave1 cp ${XML} /usr/local/hadoop/etc/hadoop/
+
+printf "${SIGN} Formatting HDFS\n"
+docker exec -t Master /usr/local/hadoop/bin/hadoop namenode -format
+
+printf "${SIGN} Executing start-all.sh\n"
+docker exec -t Master /usr/local/hadoop/sbin/start-all.sh
+
+printf "${SIGN} Executing run_sample_wordcount.sh\n"
+docker exec -t Master /data/run_sample_wordcount.sh
